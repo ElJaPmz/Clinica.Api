8aacb6eed20a5e82d78363ef2d25bb3663b515ab
DanyGit15 <githubdany@gmail.com>
2026-03-14 15:51:32 -0600

    Se agregó la entidad Paciente

---
+++ Clinica.Domain/Entities/Paciente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace Clinica.Domain.Entities
+{
+    public class Paciente
+    {
+        public int IdPaciente { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public string Telefono { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string DocumentoIdentidad { get; set; } = null!;
+
+        public string TipoPaciente { get; set; } = null!;
+
+    }
+}
