1d797e586e4f1580b1b5a76b73518ed221566f9a
DanyGit15 <githubdany@gmail.com>
2026-03-14 21:52:30 -0600

    d

---
+++ Clinica.Domain/Entities/Medico.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clinica.Domain.Entities
+{
+    public class Medico
+    {
+
+        public int Id_Medico { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public int Id_Especialidad { get; set; }
+        public int telefono { get; set; }
+        public string Correo { get; set; } = string.Empty;
+        public string Estado { get; set; } = string.Empty;
+
+
+
+    }
+}
