7b31e74c1e594c4d1cac334240bcc27eacb3e972
DanyGit15 <githubdany@gmail.com>
2026-03-22 22:10:23 -0600

    Mapping

--- Clinica.Application/Mappings/MappingProfile.cs
+++ Clinica.Application/Mappings/MappingProfile.cs
@@ -1,21 +1,28 @@
 ﻿using AutoMapper;
 using Clinica.Application.DTOs.Consultorio;
+using Clinica.Application.DTOs.Paciente;
 using Clinica.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Clinica.Application.Mappings
 {
-    public class MappingProfile: Profile
+    public class MappingProfile : Profile
     {
-        public MappingProfile() 
+        public MappingProfile()
         {
             #region Consultorios
             CreateMap<Consultorio, ConsultorioDto>();
             CreateMap<ConsultorioCrearDto, Consultorio>();
             CreateMap<ConsultorioActualizarDto, Consultorio>()
                 .ForMember(dest => dest.Id_Consultorio, opt => opt.Ignore());
+            #endregion
+
+            #region Pacientes
+            CreateMap<Paciente, PacienteDto>();
+
+            CreateMap<PacienteCrearDto, Paciente>();
+
+            CreateMap<PacienteActualizarDto, Paciente>()
+                .ForMember(dest => dest.IdPaciente, opt => opt.Ignore());
             #endregion
         }
     }
