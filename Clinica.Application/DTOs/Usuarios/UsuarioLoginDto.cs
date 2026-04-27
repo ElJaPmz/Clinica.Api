1cc343147f24331dd64c1c8c09f83e0adc89fbfd
Harvey Cuadra <Harvey Cuadra@DESKTOP-PM2NNE9>
2026-04-26 20:55:55 -0600

    "Finalización módulo clínica y corrección de roles"

--- Clinica.Application/DTOs/Usuarios/UsuarioLoginDto.cs
+++ Clinica.Application/DTOs/Usuarios/UsuarioLoginDto.cs
@@ -12,5 +12,8 @@ namespace Clinica.Application.DTOs.Usuarios
 
         [Required(ErrorMessage = "La contraseña es obligatoria para iniciar sesión.")]
         public string Password { get; set; } = string.Empty;
+        // Agrega esta línea para que aparezca en el JSON de Swagger
+        [Required(ErrorMessage = "El rol es requerido para validar el acceso.")]
+        public string Rol { get; set; } = string.Empty;
     }
 }
